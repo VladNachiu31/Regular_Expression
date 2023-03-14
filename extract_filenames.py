acc80bf485298dc272c0eea1e088b7b670c23986
VladNachiu31 <nachiu.vlad@yahoo.com>
2023-03-14 10:18:46 +0000

    new file:   .idea/.gitignore 	new file:   .idea/Regular_Expression.iml 	new file:   .idea/dbnavigator.xml 	new file:   .idea/inspectionProfiles/Project_Default.xml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   extract_email.py 	new file:   extract_filenames.py 	new file:   extract_ip.py 	new file:   extract_url.py 	new file:   extract_word_from_text.py 	new file:   files.zip 	new file:   ips.txt 	new file:   requirements.txt 	new file:   urls.txt

---
+++ extract_filenames.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import re
+
+root_dir = Path('files')
+filenames = root_dir.iterdir()
+filenames_str = [filename.name for filename in filenames]
+filenames_str
+
+
+pattern = re.compile("nov[a-z]*-(?:[1-9]|1[0-9]|20).txt", re.IGNORECASE)
+matches = [filename for filename in filenames_str if pattern.findall(filename)]
+matches
